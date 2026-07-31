1011b3eba144eecc058ab3e24e7353810dd67cc4
caltimafpfp <b0923721012@gmail.com>
2026-08-01 04:17:27 +0800

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,13 +1,16 @@
 window.SITE_CONFIG = {
-  lineUrl: "#",
-  donateUrl: "#",
-  clientUrl: "#",
-  patchUrl: "#",
-  driveUrl: "#",
-  announcements: [
-    { title: "【開服公告】死亡騎士182 正式開服資訊", date: "2025-08-15" },
-    { title: "【活動資訊】開服活動與新手說明", date: "2025-08-15" },
-    { title: "【系統公告】伺服器倍率與遊戲設定", date: "2025-08-14" },
-    { title: "【下載公告】完整主程式與更新檔", date: "2025-08-14" }
-  ]
+  guideUrl:"#",
+  newsUrl:"#",
+  downloadPageUrl:"#",
+  downloadUrl:"#",
+  lineUrl:"#",
+  donateUrl:"#",
+  discordUrl:"#",
+  serverUrl:"#",
+  featuresUrl:"#",
+  weaponsUrl:"#",
+  armorUrl:"#",
+  dollsUrl:"#",
+  mapsUrl:"#",
+  itemsUrl:"#"
 };
